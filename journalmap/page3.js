914d0e2b85444f8b5717dc24b72c26d4dfd710eb
randall.michael <randall.mcihael2002@gmail.com>
2023-12-06 16:39:31 -0700

    fibished

--- journalmap/page3.js
+++ journalmap/page3.js
@@ -1,26 +1,126 @@
-  // JavaScript to handle file input and display preview
-  const photoInput = document.getElementById('photoInput');
-  const previewContainer = document.getElementById('previewContainer');
+const photoInput = document.getElementById('photoInput');
+        const previewContainer = document.getElementById('previewContainer');
 
-  photoInput.addEventListener('change', handleFileSelect);
+        // Load saved photos on page load
+        window.addEventListener('load', loadSavedPhotos);
 
-  function handleFileSelect(event) {
-      const files = event.target.files;
+        photoInput.addEventListener('change', handleFileSelect);
 
-      // Clear existing previews
-      previewContainer.innerHTML = '';
+        function handleFileSelect(event) {
+            const files = event.target.files;
+            previewContainer.innerHTML = ''; // Clear existing previews
 
-      // Display preview for each selected file
-      for (const file of files) {
-          const reader = new FileReader();
+            for (const file of files) {
+                savePhoto(file);
+                displayPhoto(file);
+            }
+        }
 
-          reader.onload = function (e) {
-              const img = document.createElement('img');
-              img.src = e.target.result;
-              img.alt = 'Selected Photo';
-              previewContainer.appendChild(img);
-          };
+        function savePhoto(file) {
+            // Save photo data to localStorage
+            const photoData = localStorage.getItem('photos') || '[]';
+            const photos = JSON.parse(photoData);
+            photos.push({ name: file.name, caption: 'Your Caption Here' });
+            localStorage.setItem('photos', JSON.stringify(photos));
+        }
 
-          reader.readAsDataURL(file);
-      }
-  }
+        function displayPhoto(file) {
+            const reader = new FileReader();
+
+            reader.onload = function (e) {
+                const container = document.createElement('div');
+                container.classList.add('photoRow');
+
+                const img = document.createElement('img');
+                img.src = URL.createObjectURL(file);
+                img.alt = 'Selected Photo';
+                img.classList.add('photoPreview');
+
+                const caption = document.createElement('div');
+                caption.classList.add('photoCaption');
+
+                const captionInput = document.createElement('input');
+                captionInput.type = 'text';
+                captionInput.value = 'Your Caption Here';
+                captionInput.addEventListener('input', function () {
+                    updateCaption(file.name, this.value);
+                });
+
+                // Attach a new click event listener
+                img.addEventListener('click', function () {
+                    const photoName = file.name;
+                    deletePhoto(photoName);
+                    container.remove(); // Remove the container from the DOM when clicked
+                });
+
+                container.appendChild(img);
+                caption.appendChild(captionInput);
+                container.appendChild(caption);
+                previewContainer.appendChild(container);
+            };
+
+            reader.readAsDataURL(file);
+        }
+
+        function loadSavedPhotos() {
+            const photoData = localStorage.getItem('photos');
+            if (photoData) {
+                const photos = JSON.parse(photoData);
+                photos.forEach(photo => {
+                    const container = document.createElement('div');
+                    container.classList.add('photoRow');
+
+                    const img = document.createElement('img');
+                    img.src = `images/${photo.name}`; // Update the path accordingly
+                    img.alt = 'Selected Photo';
+                    img.classList.add('photoPreview');
+
+                    const caption = document.createElement('div');
+                    caption.classList.add('photoCaption');
+
+                    const captionInput = document.createElement('input');
+                    captionInput.type = 'text';
+                    captionInput.value = photo.caption || 'Your Caption Here';
+                    captionInput.addEventListener('input', function () {
+                        updateCaption(photo.name, this.value);
+                    });
+
+                    // Attach a new click event listener
+                    img.addEventListener('click', function () {
+                        const photoName = photo.name;
+                        deletePhoto(photoName);
+                        container.remove(); // Remove the container from the DOM when clicked
+                    });
+
+                    container.appendChild(img);
+                    caption.appendChild(captionInput);
+                    container.appendChild(caption);
+                    previewContainer.appendChild(container);
+                });
+            }
+        }
+
+        function deletePhoto(photoName) {
+            // Delete photo by name from localStorage
+            const photoData = localStorage.getItem('photos');
+            if (photoData) {
+                const photos = JSON.parse(photoData);
+                const updatedPhotos = photos.filter(photo => photo.name !== photoName);
+                localStorage.setItem('photos', JSON.stringify(updatedPhotos));
+            }
+        }
+
+        function updateCaption(photoName, newCaption) {
+            // Update the caption for a photo in localStorage
+            const photoData = localStorage.getItem('photos');
+            if (photoData) {
+                const photos = JSON.parse(photoData);
+                const updatedPhotos = photos.map(photo => {
+                    if (photo.name === photoName) {
+                        return { ...photo, caption: newCaption };
+                    }
+                    return photo;
+                });
+                localStorage.setItem('photos', JSON.stringify(updatedPhotos));
+            }
+        }
